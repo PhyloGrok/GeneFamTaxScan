ab88036578777e33187c1d9ddb025bd3988fe748
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-11-28 14:00:26 -0500

    Create homolog2fasta.sh

---
+++ homolog2fasta.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+outf="drosha.fasta"
+echo -n "" > $outf
+geneids=$(esearch -db homologene -query "8293" | esummary | xtract -pattern DocumentSummary -element GeneID | tr "\t" ",")
+
+for i in $(esearch -db gene -query "$geneids" | efetch -format docsum | xtract -pattern DocumentSummary -element GenomicInfoType/ChrAccVer GenomicInfoType/ChrStart GenomicInfoType/ChrStop 
+| tr "\t" ",");
+        do
+                data=($(echo $i | tr "," "\n"))
+                if [[ ${#data[@]} == 3 ]]
+                        then
+                                efetch -db nuccore -id ${data[0]} -seq_start=${data[1]} -seq_stop=${data[2]} -format fasta >> $outf
+                fi
+        done;
