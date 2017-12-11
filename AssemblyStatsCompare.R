6d33a9e482ec08da1356565970a1d7596c0dcd56
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-11 14:39:51 -0500

    Create AssemblyStatsCompare.R

---
+++ AssemblyStatsCompare.R
@@ -0,0 +1,13 @@
+##Import a .csv file of Assembly Metadata with RefSeq_category,contig_count,total_length data, output Box&Whisker plots.
+
+AStats <- read.csv("AssemblyStats.csv")
+
+library(ggplot2)
+
+ContigCount <- ggplot(AStats, aes(RefSeq_category, contig_count)) +
+geom_boxplot() + scale_y_log10()
+CongitCount
+
+AssemblyLength <- ggplot(AStats, aes(RefSeq_category, total_length)) +
+geom_boxplot() + scale_y_log10()
+AssemblyLength
