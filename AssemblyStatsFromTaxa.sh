2811f4b233daae50584a29ff471efbace8bffbd3
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-12 16:39:08 -0500

    Update AssemblyStatsFromTaxa.sh

--- AssemblyStatsFromTaxa.sh
+++ AssemblyStatsFromTaxa.sh
@@ -1,5 +1,6 @@
 ##Retrieves .csv file (no headers) assembly stats for taxa of interest.
 
 for i in `esearch -db genome -query txid9443[Orgn] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element Id`; do efetch -db assembly -id $i -format docsum | xtract -pattern DocumentSummary -element Organism SpeciesTaxid AssemblyAccession RefSeq_category AssemblyStatus -block Meta -element Stat | tr "\t" ","; done
+#To Exclude selected child taxa: esearch -db genome -query "txid33208[Orgn] NOT txid6960[Orgn] NOT txid7742[Orgn]"
 
 ##(Output: see [AssemblyStats.csv](../master/AssemblyStats.csv))  *Note column headers entered manually*
