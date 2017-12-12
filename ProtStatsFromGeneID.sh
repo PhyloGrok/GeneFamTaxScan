b320cf0348d0d0fa620eb51e8b7d914ef9213af8
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-12 12:32:31 -0500

    Update ProtStatsFromGeneID.sh

--- ProtStatsFromGeneID.sh
+++ ProtStatsFromGeneID.sh
@@ -1,3 +1,5 @@
-##!/bin/bash
 ##Retrieves relevant protein info in .csv format starting with "ortholog group ID"
-for i in `esearch -db gene -query "ortholog_gene_29102[group]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption Title SourceDb Slen | tr "\t" ","; done
+##Note that "Title" entries are a bit flaky - some sequences say "Drosha, partial".  Semi-colon delimiters were used instead, but a couple prots still needed manual revision
+#!/bin/bash
+
+for i in `esearch -db gene -query "ortholog_gene_29102[group]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption Title SourceDb Slen | tr "\t" ";"; done
