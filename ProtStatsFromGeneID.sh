1281ac25816fa12523d9b1ddd50abaf3471f5d42
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-12 10:53:44 -0500

    Update ProtStatsFromGeneID.sh

--- ProtStatsFromGeneID.sh
+++ ProtStatsFromGeneID.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-
+##!/bin/bash
+##Retrieves relevant protein info in .csv format starting with "ortholog group ID"
 for i in `esearch -db gene -query "ortholog_gene_29102[group]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption Title SourceDb Slen | tr "\t" ","; done
