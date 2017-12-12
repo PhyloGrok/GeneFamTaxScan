ed212d6991f00e893f4980d06afbc85e9734a44c
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-12 14:06:08 -0500

    Update AssemblyStatsFromTaxa.sh

--- AssemblyStatsFromTaxa.sh
+++ AssemblyStatsFromTaxa.sh
@@ -2,5 +2,4 @@
 
 for i in `esearch -db genome -query txid9443[Orgn] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element Id`; do efetch -db assembly -id $i -format docsum | xtract -pattern DocumentSummary -element Organism SpeciesTaxid AssemblyAccession RefSeq_category AssemblyStatus -block Meta -element Stat | tr "\t" ","; done
 
-
 ##(Output: see [AssemblyStats.csv](../master/AssemblyStats.csv))  *Note column headers entered manually*
