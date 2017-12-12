f3fe0cd6067844dea8abbf08f80552b12d57a9a1
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-12 14:05:50 -0500

    Update and rename AssemblyStatsFromTaxa.md to AssemblyStatsFromTaxa.sh

--- AssemblyStatsFromTaxa.md
+++
@@ -1,6 +0,0 @@
-Script retrieves .csv file (no headers) assembly stats for taxa of interest.
-```
-$ esearch -db genome -query txid9443[Orgn] |elink -target assembly | esummary | xtract -pattern DocumentSummary -element Id Organism SpeciesTaxid AssemblyAccession RefSeq_category AssemblyStatus -block Meta -element Stat | tr "\t" "," > Primates.csv
-```
-
-(see [PrimatesAssemblyStats.csv](../master/PrimatesAssemblyStats.csv))  *Note column headers entered manually*
---
+++ AssemblyStatsFromTaxa.sh
@@ -0,0 +1,6 @@
+##Retrieves .csv file (no headers) assembly stats for taxa of interest.
+
+for i in `esearch -db genome -query txid9443[Orgn] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element Id`; do efetch -db assembly -id $i -format docsum | xtract -pattern DocumentSummary -element Organism SpeciesTaxid AssemblyAccession RefSeq_category AssemblyStatus -block Meta -element Stat | tr "\t" ","; done
+
+
+##(Output: see [AssemblyStats.csv](../master/AssemblyStats.csv))  *Note column headers entered manually*
