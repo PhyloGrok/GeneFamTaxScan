761f4058d2c8c818bffa9bb4c30ffc46c399fe8c
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-12 10:52:29 -0500

    Update and rename ProtSlenFromGeneID.md to ProtStatsFromGeneID.sh

---
+++ ProtStatsFromGeneID.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for i in `esearch -db gene -query "ortholog_gene_29102[group]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption Title SourceDb Slen | tr "\t" ","; done
