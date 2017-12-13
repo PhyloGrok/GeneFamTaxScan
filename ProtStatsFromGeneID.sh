01487949c72a6ba57f82ecf08565bc60795ef978
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-13 16:13:51 -0500

    Update ProtStatsFromGeneID.sh

--- ProtStatsFromGeneID.sh
+++ ProtStatsFromGeneID.sh
@@ -2,9 +2,6 @@
 ##"Title" entries can be flaky - some sequences say "Drosha, partial".  Semi-colon delimiters were used instead, but a couple prots still needed manual revision
 #!/bin/bash
 
-for i in `esearch -db gene -query "ortholog_gene_29102[group]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption Title SourceDb Slen | tr "\t" ";"; done
-
+for i in `esearch -db gene -query "ortholog_gene_29102[group] AND txid9989[Orgn]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ";"; done
 ##Get Rodent Specific Drosha ortholog data
-#for i in `esearch -db gene -query "ortholog_gene_29102[group] AND txid9989[Orgn]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ";"; done
-
-##Will NOT work for inveretbrate taxa - orthology groups only for Inverts.  BLAST implementation may be necessary.
+##Will NOT work for inveretbrate taxa - orthology groups only for Verts.  BLAST implementation may be necessary.
