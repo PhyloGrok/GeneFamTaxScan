cb67d7637a7a93b500ca055fc6fb3955a6c3cce6
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-13 11:17:24 -0500

    Update and rename AssemblyRefseqFastasByTax.md to AssemblyRefseqFastasByTax.sh

---
+++ AssemblyRefseqFastasByTax.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#esearch -db genome -query txidXXX[Organism:exp] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element #Organism FtpPath_RefSeq
+
+#Return RefSeq .gz files:
+
+wget `esearch -db genome -query txidXXX[Organism:exp] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element FtpPath_RefSeq | awk -F"/" '{print $0"/"$NF"_genomic.fna.gz"}'`
