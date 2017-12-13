cfe629338f7decf47a6603b0dda0698c9677cbda
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-13 13:36:20 -0500

    Update ProtStatsFromGeneID.sh

--- ProtStatsFromGeneID.sh
+++ ProtStatsFromGeneID.sh
@@ -1,5 +1,7 @@
 ##Retrieves relevant protein info in .csv format starting with "ortholog group ID"
-##Note that "Title" entries are a bit flaky - some sequences say "Drosha, partial".  Semi-colon delimiters were used instead, but a couple prots still needed manual revision
+##"Title" entries can be flaky - some sequences say "Drosha, partial".  Semi-colon delimiters were used instead, but a couple prots still needed manual revision
 #!/bin/bash
 
 for i in `esearch -db gene -query "ortholog_gene_29102[group]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption Title SourceDb Slen | tr "\t" ";"; done
+
+##Will NOT work for inveretbrate taxa - orthology groups only for Inverts.  BLAST implementation may be necessary.
