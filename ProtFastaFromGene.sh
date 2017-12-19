d1b687102e101324314e4c483a039a583d309c7d
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-19 14:13:29 -0500

    Update ProtFastaFromGene.sh

--- ProtFastaFromGene.sh
+++ ProtFastaFromGene.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ##Retrieves RefSeq Protein fastas from a GeneId input.
 
-for i in `esearch -db gene -query "14000" | elink -target protein | esummary | xtract -pattern DocumentSummary -if SourceDb -equals "refseq" -element Caption`; do efetch -db protein -id $i -format fasta; done
+for i in `esearch -db gene -query "$1" | elink -target protein | esummary | xtract -pattern DocumentSummary -if SourceDb -equals "refseq" -element Caption`; do efetch -db protein -id $i -format fasta; done
