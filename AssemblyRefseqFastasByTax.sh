9d0f4273e3d44e7c09eaee09b2a9716a5cfebdf4
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-19 14:15:28 -0500

    Update AssemblyRefseqFastasByTax.sh

--- AssemblyRefseqFastasByTax.sh
+++ AssemblyRefseqFastasByTax.sh
@@ -2,5 +2,4 @@
 #esearch -db genome -query txidXXX[Organism:exp] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element #Organism FtpPath_RefSeq
 
 #Return RefSeq .gz files:
-
 wget `esearch -db genome -query txid$1[Organism:exp] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element FtpPath_RefSeq | awk -F"/" '{print $0"/"$NF"_genomic.fna.gz"}'`
