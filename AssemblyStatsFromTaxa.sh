bd11064834897a9bb54500606e2e4a186f94bd32
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2017-12-19 13:19:42 -0500

    Update AssemblyStatsFromTaxa.sh

--- AssemblyStatsFromTaxa.sh
+++ AssemblyStatsFromTaxa.sh
@@ -1,6 +1,9 @@
-##Retrieves .csv file (no headers) assembly stats for taxa of interest.
+##Retrieves .csv file with assembly stats for taxa of interest.
 
-for i in `esearch -db genome -query txid9443[Orgn] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element Id`; do efetch -db assembly -id $i -format docsum | xtract -pattern DocumentSummary -element Organism SpeciesTaxid AssemblyAccession RefSeq_category AssemblyStatus -block Meta -element Stat | tr "\t" ","; done
+echo "Id,Organism,SpeciesTaxid,AssemblyAccession,RefSeq_category,AssemblyStatus,alt_loci_count,chromosome_count,contig_count,contig_l50,contig_n50,non_chromosome_replicon_count,replicon_count,scaffold_count(all),scaffold_count(placed),scaffold_count(unlocalized),scaffold_count(unplaced),scaffold_l50,scaffold_n50,total_length,ungapped_length" > AssemblyStats.csv
+
+for i in `esearch -db genome -query txid$1[Orgn] | elink -target assembly | esummary | xtract -pattern DocumentSummary -element Id`; do efetch -db assembly -id $i -format docsum | xtract -pattern DocumentSummary -element Id Organism SpeciesTaxid AssemblyAccession RefSeq_category AssemblyStatus -block Meta -element Stat | tr "\t" ","; done >> AssemblyStats.csv
+
 #To Exclude selected child taxa: esearch -db genome -query "txid33208[Orgn] NOT txid6960[Orgn] NOT txid7742[Orgn]"
 #$ for i in `esearch -db genome -query "txid33208[Orgn] NOT txid6960[Orgn] NOT txid7742[Orgn]" | elink -target assembly | esummary | xtract -pattern DocumentSummary -element Id`; do efetch -db assembly -Id $i -format docsum | xtract -pattern DocumentSummary -element Id Organism SpeciesTaxid AssemblyAccession RefSeq_category AssemblyStatus -block Meta -element Stat | tr "\t" ","; done
 ##(Output: see [AssemblyStats.csv](../master/AssemblyStats.csv))  *Note column headers entered manually*
