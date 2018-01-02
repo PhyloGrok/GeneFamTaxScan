7f0b1409be61d4f4d219932a0d316862482c5420
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2018-01-02 15:51:48 -0500

    Update ProtStatsFromGeneID.sh

--- ProtStatsFromGeneID.sh
+++ ProtStatsFromGeneID.sh
@@ -1,7 +1,7 @@
 ##Retrieves relevant protein info in .csv format starting with "ortholog group ID"
-##"Title" has a lot of "," and tabs, don't retrieve those
+
 #!/bin/bash
 
-for i in `esearch -db gene -query "ortholog_gene_$1[group] AND txid$2[Orgn]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ","; done
-##Get Rodent Specific Drosha ortholog data
+for i in `esearch -db gene -query "ortholog_gene_$1[group] AND txid$2[Orgn]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ","; done >> orthology_$1_$2.csv
+
 ##Will NOT work for inveretbrate taxa - orthology groups only for Verts.  BLAST implementation may be necessary.
