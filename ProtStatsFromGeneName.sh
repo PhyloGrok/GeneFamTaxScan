b80f8e9e433378e6d520b6d5161708d024306e33
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2018-07-26 11:34:58 -0400

    Create ProtStatsFromGeneName.sh

---
+++ ProtStatsFromGeneName.sh
@@ -0,0 +1,7 @@
+##Retrieves relevant protein info in .csv format starting with "ortholog group ID"
+
+#!/bin/bash
+
+for i in `esearch -db gene -query "gene_name$1[TITL] AND txid$2[Orgn]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ","; done >> orthology_$1_$2.csv
+
+##For obtaining "gene" from annotation name.
