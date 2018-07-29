c21f22dbd70c6d2d6c217a53c39025d9a88b9033
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2018-07-29 14:58:52 -0400

    Update ProtStatsFromGeneName.sh

--- ProtStatsFromGeneName.sh
+++ ProtStatsFromGeneName.sh
@@ -2,6 +2,6 @@
 
 #!/bin/bash
 
-for i in `esearch -db gene -query "gene_name$1[GENE] AND txid$2[ORGN]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ","; done >> orthology_$1_$2.csv
+for i in `esearch -db gene -query "$1[GENE] AND txid$2[ORGN]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ","; done >> orthology_$1_$2.csv
 
 ##For obtaining "gene" from annotation name.
