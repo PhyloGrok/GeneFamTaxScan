3d433ff38115eac170511888e19404214b9e5bcb
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2018-07-29 15:59:25 -0400

    Update and rename ProtStatsFromGeneName.sh to ProtStatsFromGeneWord.sh

--- ProtStatsFromGeneName.sh
+++
@@ -1,7 +0,0 @@
-##Retrieves relevant protein info in .csv format starting with "ortholog group ID"
-
-#!/bin/bash
-
-for i in `esearch -db protein -query "$1[PROT] AND txid$2[ORGN]"  | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ","; done >> orthology_$1_$2.csv
-
-##For obtaining "gene" from annotation name.
---
+++ ProtStatsFromGeneWord.sh
@@ -0,0 +1,7 @@
+##Retrieves relevant protein info in .csv format starting with "ortholog group ID"
+
+#!/bin/bash
+
+for i in `esearch -db gene -query "$1[WORD] AND txid$2[ORGN]"  | elink -target protein | esummary | xtract -pattern DocumentSummary -element Caption`; do efetch -db protein -id $i -format docsum | xtract -pattern DocumentSummary -element Caption SourceDb Slen | tr "\t" ","; done >> orthology_$1_$2.csv
+
+##For obtaining "gene" from annotation name.
